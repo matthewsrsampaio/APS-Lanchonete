aac46d696d4d4ae7618f4376ba10d9747f27d1a5
Matthews R. Sampaio <85257122+matthewsrsampaio@users.noreply.github.com>
2022-10-23 18:28:59 -0300

    Delete Aula_13SET directory

--- Aula_13SET/src/Exercicio11_Banco/BancoMain.java
+++
@@ -1,9 +0,0 @@
-package Exercicio11_Banco;
-
-public class BancoMain {
-    public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario(1000.0);
-        funcionario.aumentoSalario(30);
-        System.out.println("Novo salario e de: "+funcionario.getSalario());
-    }
-}
